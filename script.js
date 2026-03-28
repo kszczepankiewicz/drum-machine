1a744ba15bd497769fa5bbf902a3258ef7e75925
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-28 09:20:10 +0100

    feat: add structure

---
+++ script.js
@@ -0,0 +1,10 @@
+const $ = id => document.getElementById(id);
+
+const padBank = $('pad-bank');
+
+let text = padBank.innerHTML;
+const pads = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+const fileNames = ['Heater-1', 'Heater-2', 'Heater-3', 'Heater-4_1', 'Heater-6', 'Dsc_Oh', 'Kick_n_Hat', 'RP4_KICK_1', 'Cev_H2']
+const link = 'https://cdn.freecodecamp.org/curriculum/drum/';
+pads.forEach((p, i) => text += `<button id="${p}" class="drum-pad">${p}<audio id="${p}" class="clip" src="${link + fileNames[i] + '.mp3'}"></audio></button>`);
+padBank.innerHTML = text;
