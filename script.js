ec99109b2166132989249b970133fe8ed94ad36a
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-28 16:10:04 +0100

    refactor: remove unused variables text and buttons

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const $ = id => document.getElementById(id);
 
 const container = $('container')
 
-let text = container.innerHTML;
 const pads = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
 const fileNames = ['Heater-1', 'Heater-2', 'Heater-3', 'Heater-4_1', 'Heater-6', 'Dsc_Oh', 'Kick_n_Hat', 'RP4_KICK_1', 'Cev_H2']
 const link = 'https://cdn.freecodecamp.org/curriculum/drum/';
@@ -31,7 +30,6 @@ pads.forEach((pad, i) => {
     button.addEventListener('click', (e) => playSound(i))
 });
 
-const buttons = document.querySelectorAll('button');
 const audios = document.querySelectorAll('audio');
 
 // display audio name
