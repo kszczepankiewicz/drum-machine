5f8fd3fa40d0a5b486a43a12467b6fde432b9e7b
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-28 09:39:21 +0100

    feat: add styling

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const $ = id => document.getElementById(id);
 
 const padBank = $('pad-bank');
+const container = $('container')
 
-let text = padBank.innerHTML;
+let text = container.innerHTML;
 const pads = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
 const fileNames = ['Heater-1', 'Heater-2', 'Heater-3', 'Heater-4_1', 'Heater-6', 'Dsc_Oh', 'Kick_n_Hat', 'RP4_KICK_1', 'Cev_H2']
 const link = 'https://cdn.freecodecamp.org/curriculum/drum/';
 pads.forEach((p, i) => text += `<button id="${p}" class="drum-pad">${p}<audio id="${p}" class="clip" src="${link + fileNames[i] + '.mp3'}"></audio></button>`);
-padBank.innerHTML = text;
+container.innerHTML = text;
