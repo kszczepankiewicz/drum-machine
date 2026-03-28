8f98359d4166524676c03deaa7aedeb3a93ab74c
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-28 14:49:12 +0100

    fix: avoid innerHTML

--- script.js
+++ script.js
@@ -7,8 +7,21 @@ let text = container.innerHTML;
 const pads = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
 const fileNames = ['Heater-1', 'Heater-2', 'Heater-3', 'Heater-4_1', 'Heater-6', 'Dsc_Oh', 'Kick_n_Hat', 'RP4_KICK_1', 'Cev_H2']
 const link = 'https://cdn.freecodecamp.org/curriculum/drum/';
-pads.forEach((p, i) => text += `<button id="${p}" class="drum-pad">${p}<audio id="${p}" class="clip" src="${link + fileNames[i] + '.mp3'}"></audio></button>`);
-container.innerHTML = text;
+
+pads.forEach((pad, i) => {
+    const button = document.createElement('button');
+    button.id = pad;
+    button.classList.add('drum-pad');
+    button.textContent = pad;
+
+    const audio = document.createElement('audio');
+    audio.id = pad;
+    audio.classList.add('clip');
+    audio.src = link + fileNames[i] + '.mp3';
+
+    button.appendChild(audio);
+    container.appendChild(button);
+});
 
 const buttons = document.querySelectorAll('button');
 const audios = document.querySelectorAll('audio');
